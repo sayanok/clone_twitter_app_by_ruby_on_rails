fa7b7f8656609791fc0c8c66002403881377c767
sayanok <s.kasai@shiza1.com>
2026-03-28 09:46:36 +0900

    Add user model and migration, update Gemfile for bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_22_070616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string "account", null: false
+    t.string "user_name", null: false
+    t.text "description"
+    t.string "password_digest", null: false
+    t.date "birth_of_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account"], name: "index_users_on_account", unique: true
+  end
 end
