e902ab28666fa67b7854a3a8d25261052d235383
sayanok <s.kasai@shiza1.com>
2026-03-28 16:11:41 +0900

    Update schema and migration for Tweets table
    - Added a new Tweets table with user_id and tweet_text fields.
    - Included timestamps for tweet creation and updates.
    - Established a foreign key relationship between tweets and users.
    - Updated schema version to reflect the new migration.

--- db/migrate/20260328013113_create_tweets.rb
+++ db/migrate/20260328013113_create_tweets.rb
@@ -1,7 +1,10 @@
 class CreateTweets < ActiveRecord::Migration[7.2]
   def change
     create_table :tweets do |t|
+      t.integer :user_id
+      t.string :tweet_text, limit: 140
       t.timestamps
     end
+    add_foreign_key :tweets, :users
   end
 end
