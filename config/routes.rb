42b31cd19f7145dbb9df57587b4fbffb3b122c65
sayanok <s.kasai@shiza1.com>
2026-03-28 10:16:18 +0900

    Refactor Gemfile and routes for consistency; standardize gem syntax and format routes for improved readability.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
   # root "posts#index"
 
-  resources :users, only:[:show]
-  get '/:account', to: 'users#show', as: :account
+  resources :users, only: [ :show ]
+  get "/:account", to: "users#show", as: :account
 end
