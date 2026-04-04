cc18970e5a9c64049c1c75f47d02bd0ec2b4852f
sayanok <s.kasai@shiza1.com>
2026-04-04 22:08:46 +0900

    Update routes to include account-specific tweet display
    - Removed the duplicate show action from tweets resources.
    - Added a new route for displaying tweets by account, enhancing the routing structure for better organization.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "tweets#index"
-  resources :tweets, only: [ :index, :show, :new, :create, :destroy ]
+  resources :tweets, only: [ :index, :new, :create, :destroy ]
+  get "/:account/tweets/:id", to: "tweets#show", as: :account_tweet
 
   resources :users, only: [ :show ]
   get "/:account", to: "users#show", as: :account
