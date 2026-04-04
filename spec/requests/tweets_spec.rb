0becc304739bcc93a727d6e48e09053ef49c401e
sayanok <s.kasai@shiza1.com>
2026-04-04 20:21:48 +0900

    Remove unnecessary whitespace in tweets_spec.rb for cleaner code.

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -13,7 +13,6 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/tweets", type: :request do
-  
   # This should return the minimal set of attributes required to create a valid
   # Tweet. As you add validations to Tweet, be sure to
   # adjust the attributes here as well.
